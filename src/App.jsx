bf62058a91083ad3e9140ddccf120cd6907cc22f
soppmocha <ksst5209@naver.com>
2026-06-01 17:03:37 +0900

    pages 수정 테ㅔ스트

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
                     <img src={viteLogo} className="vite" alt="Vite logo"/>
                 </div>
                 <div>
-                    <h1>Get started</h1>
+                    <h1>크아악롤랑이새끼가중년주제에크윽롤랑</h1>
                     <p>
                         Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
                     </p>

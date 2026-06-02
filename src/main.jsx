b16b32f7cb3f4ac47923d42160f5f53b04ab7201
soppmocha <ksst5209@naver.com>
2026-06-03 00:53:58 +0900

    hashrouter로 수정(github pages용)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* 💡 핵심: 배포 환경일 때만 자동으로 앞에 /ilbulgae를 붙여서 라우팅을 시작합니다. */}
-        <BrowserRouter basename={import.meta.env.DEV ? "" : "/ilbulgae"}>
+        <HashRouter basename={import.meta.env.DEV ? "" : "/ilbulgae"}>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
